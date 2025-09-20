dd3cafcdd3119f29b6e23a8240c5f566d799678e
Thomas <thomas.sugiarto04@gmail.com>
2025-09-20 17:25:12 +0700

    first commit

--- blueprints/superadmin/routes.py
+++ blueprints/superadmin/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_required, current_user
-from sklearn.conftest import wraps
+from functools import wraps
 from werkzeug.security import generate_password_hash
 from blueprints import admin
 from extensions import db
